c84199a35e05a005c27a155c10b0b1bb8c1612cf
Hitesh saini <hiteshsaini0277@gmail.com>
2026-04-10 20:55:01 +0530

    day1 of logic building in development

---
+++ TODO APP/script.js
@@ -0,0 +1,15 @@
+let input=document.getElementById("todo-input");
+let addBtn=document.getElementById("add-btn");
+let todoList=document.getElementById("todo-list");
+
+
+addBtn.addEventListener("click",function(){
+         console.log("Button Clicked");
+         let task = input.value;
+         if(task){
+            let li=document.createElement("li");
+            li.textContent=task;
+            todoList.appendChild(li);
+            input.value="";
+         }
+});
