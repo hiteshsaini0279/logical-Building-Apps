03e55b410301589ae740298e94779c567d183818
Hitesh saini <hiteshsaini0277@gmail.com>
2026-04-13 17:00:01 +0530

    day3 of logic build in frontrd side

--- TODO APP/script.js
+++ TODO APP/script.js
@@ -3,12 +3,28 @@ let addBtn=document.getElementById("add-btn");
 let todoList=document.getElementById("todo-list");
 
 addBtn.addEventListener("click",function(){
-         console.log("Button Clicked");
          let task = input.value;
+         if(task.trim() === "") {
+              return;
+            }
          if(task){
-            let li=document.createElement("li");
+           let li=document.createElement("li");
             li.textContent=task;
-            todoList.appendChild(li);
-            input.value="";
-         }
+            let deleteBtn=document.createElement("button");
+            deleteBtn.classList.add("delete-btn");
+             deleteBtn.textContent="Delete";
+             deleteBtn.addEventListener("click",function(){
+                   todoList.removeChild(li);
+             })
+               li.appendChild(deleteBtn);
+   let completeBtn=document.createElement("button");
+          completeBtn.classList.add("completed-btn");
+               completeBtn.textContent="Done!";
+completeBtn.addEventListener("click",function(){
+   li.style.textDecoration="line-through";
+}) 
+li.appendChild(completeBtn);
+todoList.appendChild(li);  
+input.value="";
+      }
 });
