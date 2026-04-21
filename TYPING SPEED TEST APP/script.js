13e43fb6bcbb78156758a0aaf839c586117bff69
Hitesh saini <hiteshsaini0277@gmail.com>
2026-04-21 08:45:54 +0530

    day9 of logic BUiding in js

---
+++ TYPING SPEED TEST APP/script.js
@@ -0,0 +1,6 @@
+const startBtn= document.getElementById("start-btn");
+const textDisplay= document.getElementById("text");
+const inputArea= document.getElementById("input");
+const timerDisplay= document.getElementById("timer");
+const resultDisplay= document.querySelector(".result"); 
+let timeLeft= 60;
