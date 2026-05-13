8b3600cd1be789c2eb7bfbcb320c0ace01260dce
Hitesh saini <hiteshsaini0277@gmail.com>
2026-05-13 13:04:01 +0530

    day 18 of logic buiding apps in frontend

---
+++ CALCULATOR/app.js
@@ -0,0 +1,34 @@
+let display = document.getElementById("display");
+
+let buttons = Array.from(document.getElementsByClassName("btn"));
+
+
+
+
+buttons.map(button => {
+
+    button.addEventListener("click", (e) => {
+
+        switch (e.target.innerText) {
+
+            case "Clr":
+                display.value = "";
+                break;
+
+            case "=":
+                try {
+                    display.value = eval(display.value);
+                } catch {
+                    display.value = "Error";
+                }
+                break;
+
+            default:
+                display.value += e.target.innerText;
+        }
+
+    });
+
+});
+
+
